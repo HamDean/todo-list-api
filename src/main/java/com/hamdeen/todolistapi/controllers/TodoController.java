766d4ce15ce63b230beeee69235b71bf1293f9a5
Hamdeen <hamdeen247@gmail.com>
2026-05-20 14:31:45 +0000

    Validate `AddTodoRequest` in controller using `@Valid` annotation

--- src/main/java/com/hamdeen/todolistapi/controllers/TodoController.java
+++ src/main/java/com/hamdeen/todolistapi/controllers/TodoController.java
@@ -2,15 +2,11 @@ package com.hamdeen.todolistapi.controllers;
 
 import com.hamdeen.todolistapi.dtos.AddTodoRequest;
 import com.hamdeen.todolistapi.dtos.TodoDto;
-import com.hamdeen.todolistapi.entities.Todo;
 import com.hamdeen.todolistapi.exceptions.TodoNotFoundException;
-import com.hamdeen.todolistapi.mappers.TodoMapper;
-import com.hamdeen.todolistapi.repositories.TodoRepository;
 import com.hamdeen.todolistapi.services.TodoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -25,7 +21,7 @@ public class TodoController {
 
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(
-            @RequestBody AddTodoRequest request,
+            @Valid @RequestBody AddTodoRequest request,
             UriComponentsBuilder uriComponentsBuilder
     ) {
         var todoDto = todoService.addTodo(request);
